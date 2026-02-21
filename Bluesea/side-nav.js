36b9baf3b18686246b352c102b8e34a1a3eab730
Right Fix <righteousnessude@gmail.com>
2026-02-21 13:00:39 +0100

    Update side-nav.js

--- Bluesea/side-nav.js
+++ Bluesea/side-nav.js
@@ -4,9 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
   getUserNav();
 });
 
-// const API_BASE = "https://api.blueseamobile.com.ng";
-
-const API_BASE = "http://157.230.168.63:8000";
+const API_BASE = "https://api.blueseamobile.com.ng";
 
 // const API_BASE = "http://127.0.0.1:8000";
 
