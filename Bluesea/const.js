872e7c04176d11aaeecf7d332ca916c00a3cdc7e
Right Fix <righteousnessude@gmail.com>
2026-02-21 10:24:48 +0100

    Update const.js

--- Bluesea/const.js
+++ Bluesea/const.js
@@ -1,2 +1,4 @@
-const API_BASE = "https://api.blueseamobile.com.ng";  
+// const API_BASE = "https://api.blueseamobile.com.ng";   
+const API_BASE = "http://157.230.168.63:8000";
 // const API_BASE = "http://127.0.0.1:8000";
+
