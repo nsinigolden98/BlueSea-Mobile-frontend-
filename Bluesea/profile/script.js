691e5c094dc96211e5f4fc595201e0da0d1b29da
goldennsini <nsinigolden1@gmail.com>
2026-02-25 18:52:32 +0100

    Implement copyText function for clipboard access
    Added a function to copy text to clipboard.

--- Bluesea/profile/script.js
+++ Bluesea/profile/script.js
@@ -49,3 +49,9 @@ imageInput.addEventListener("change", async () => {
 
 
 });
+
+function copyText(id) {
+    const text = document.getElementById(id).textContent;
+    navigator.clipboard.writeText(text);
+    showToast("Copied to clipboard");
+}
