845a3c6e4bb78417ef91078f213600b9243810a1
RightFix <righteousnessude@gmail.com>
2026-04-05 20:31:28 +0000

    debug

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -11,18 +11,7 @@ import { Eye, EyeOff, Lock, Mail, User, Phone } from 'lucide-react';
 import { TOKEN, postRequest,ENDPOINTS } from '@/types'
 import { ForgotPasswordModal } from '@/components/ui-custom/AuthModal';
 import {GoogleLogin} from '@react-oauth/google';
-// Google Icon Component
-/* function GoogleIcon({ className }: { className?: string }) {
-  return (
-    <svg className={className} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-      <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"/>
-      <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"/>
-      <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05"/>
-      <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"/>
-    </svg>
-  );
-}
- */
+
 
 type AuthMode = 'login' | 'signup';
 
@@ -145,17 +134,7 @@ export function AuthPage() {
 
             {/* Google Sign In Button */}
            
-{/*             <button
-              type="button"
-              onClick= {googleLogin}
-              disabled={loading}
-              className="w-full flex items-center justify-center gap-3 px-4 py-3 rounded-xl border border-slate-200 dark:border-slate-700 bg-white dark:bg-slate-800 hover:bg-slate-50 dark:hover:bg-slate-700 transition-all mb-4"
-            >
-              <GoogleIcon className="w-5 h-5" />
-              <span className="text-sm font-medium text-slate-700 dark:text-slate-200">
-                Continue with Google
-              </span>
-            </button> */}
+
             <GoogleLogin 
             onSuccess = {googleLogin}
             onError={()=>{console.log("Google Login error")}}
@@ -164,7 +143,7 @@ export function AuthPage() {
             theme="filled_blue"
             text= {mode === 'login' ? "signin_with" : "signup_with"}
             shape="pill"
-            width={400}
+            width={380}
             logo_alignment="left"
             
             />
