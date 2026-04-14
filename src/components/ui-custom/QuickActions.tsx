d02f519821ac5614c9f710169543e4f5b93da155
goldennsini <nsinigolden1@gmail.com>
2026-04-14 06:45:11 +0100

    Fix path casing for Pay Bills action

--- src/components/ui-custom/QuickActions.tsx
+++ src/components/ui-custom/QuickActions.tsx
@@ -36,7 +36,7 @@ const actions: QuickAction[] = [
     id: 'bills', 
     label: 'Pay Bills', 
     icon: Lightbulb, 
-    path: '/LightBills',
+    path: '/light-bills',
     color: 'from-teal-500 to-teal-600'
   },
 ];
