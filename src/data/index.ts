4f7b3314eabbb11a7bf8e50cf4fc27e4bd378bf8
goldennsini <nsinigolden1@gmail.com>
2026-04-27 17:36:08 +0100

    update index.ts

--- src/data/index.ts
+++ src/data/index.ts
@@ -4,8 +4,8 @@ import { ENDPOINTS, getRequest} from '@/types';
 export const navItems: NavItem[] = [
   { id: 'dashboard', label: 'Dashboard', icon: 'LayoutGrid', path: '/dashboard' },
   { id: 'wallet', label: 'Wallet', icon: 'Wallet', path: '/wallet' },
-  { id: 'airtime', label: 'Buy Airtime', icon: 'Smartphone', path: '/airtime' },
-  { id: 'data', label: 'Buy Data', icon: 'Wifi', path: '/data' },
+  { id: 'airtime data', label: 'Buy Airtime & Data', icon: 'Smartphone', path: '/airtime' },
+//  { id: 'data', label: 'Buy Data', icon: 'Wifi', path: '/data' },
   { id: 'marketplace', label: 'Market Place', icon: 'Store', path: '/marketplace' },
   { id: 'services', label: 'Services', icon: 'Globe', path: '/services' },
   { id: 'more-services', label: 'More Services', icon: 'Grid3X3', path: '/more-services' },
