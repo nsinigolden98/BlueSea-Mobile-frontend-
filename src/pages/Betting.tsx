d5b5c2c214292575635c4e6d54557efeeb1b0e7c
goldennsini <nsinigolden1@gmail.com>
2026-04-30 11:23:29 +0100

    update Betting.tsx

--- src/pages/Betting.tsx
+++ src/pages/Betting.tsx
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect } from 'react';
-import { Sidebar, Header, TransactionModal, PinModal, Toast } from '@/components/ui-custom';
+import { Sidebar, TransactionModal, PinModal, Toast } from '@/components/ui-custom';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
