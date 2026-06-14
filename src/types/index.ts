99f31d863834660780188287fff7daf1710ff845
goldennsini <nsinigolden1@gmail.com>
2026-06-14 14:33:21 +0100

    update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -863,7 +863,7 @@ export interface RentalUnit {
 
 
 export interface Transaction {
-  id: Number; // Fixed id number vs string conflict to safely enable implicit interface merging
+  id: number; // Fixed id number vs string conflict to safely enable implicit interface merging
   transaction_type: 'CREDIT' | 'DEBIT';
   amount: number;
   description: string;
