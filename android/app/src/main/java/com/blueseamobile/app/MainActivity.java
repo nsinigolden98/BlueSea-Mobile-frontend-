fbd38ee9f38574631659350b5eb4e95028601bdf
goldennsini <nsinigolden1@gmail.com>
2026-07-11 15:18:28 +0100

    chore: added native capacitor android wrappers

---
+++ android/app/src/main/java/com/blueseamobile/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.blueseamobile.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.blueseamobile.app',
+  appName: 'BlueSea Mobile',
+  webDir: 'dist'
+};
+
+export default config;
